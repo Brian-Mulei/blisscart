6c9920473d328d792daec3f3e0b05acc4e4fc1be
Brian-Mulei <brianmulei0@gmail.com>
2024-07-26 16:25:58 +0300

    aws

--- src/main/java/com/mulei/blisscart/config/AmazonServiceConfig.java
+++ src/main/java/com/mulei/blisscart/config/AmazonServiceConfig.java
@@ -1,8 +1,6 @@
 package com.mulei.blisscart.config;
 
 import org.springframework.beans.factory.annotation.Value;
-
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
